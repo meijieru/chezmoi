71304635bd8ba5bf7f59e97ceaa0bdc672ace239
meijieru <meijieru@gmail.com>
2021-11-11 02:21:42 -0500

    [nvim] trouble.nvim

--- dot_config/lvim/lua/modules/completion/config.lua
+++ dot_config/lvim/lua/modules/completion/config.lua
@@ -4,4 +4,21 @@ function config.telescope_luasnip()
   require("telescope").load_extension "luasnip"
 end
 
+function config.trouble()
+  require("trouble").setup {
+    use_lsp_diagnostic_signs = true,
+  }
+end
+
+if lvim.builtin.telescope.active then
+  local function _open_with_trouble(prompt_bufnr, _mode)
+    -- defer trouble loading
+    local trouble = require "trouble.providers.telescope"
+    return trouble.open_with_trouble(prompt_bufnr, _mode)
+  end
+
+  lvim.builtin.telescope.defaults.mappings.i["<C-t>"] = _open_with_trouble
+  lvim.builtin.telescope.defaults.mappings.n["<C-t>"] = _open_with_trouble
+end
+
 return config
--- dot_config/lvim/lua/modules/completion/plugins.lua
+++ dot_config/lvim/lua/modules/completion/plugins.lua
@@ -7,4 +7,13 @@ completion["benfowler/telescope-luasnip.nvim"] = {
   disable = not lvim.builtin.telescope.active,
 }
 
+completion["folke/trouble.nvim"] = {
+  cmd = { "Trouble", "TroubleToggle", "TroubleRefresh" },
+  module = {
+    "trouble.providers.telescope",
+  },
+  config = conf.trouble,
+  disable = false,
+}
+
 return completion
