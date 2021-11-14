dd1e14d6dbe5c97e87ad777c87f28f7274941b2f
meijieru <meijieru@gmail.com>
2021-11-14 01:53:12 -0500

    [nvim] quit_on_open for nvimtree

--- dot_config/lvim/lua/modules/ui/lvim.lua
+++ dot_config/lvim/lua/modules/ui/lvim.lua
@@ -52,6 +52,7 @@ function M.setup()
 
   lvim.builtin.nvimtree.setup.view.side = "left"
   lvim.builtin.nvimtree.show_icons.git = 0
+  lvim.builtin.nvimtree.quit_on_open = 1
 
   -- lvim.builtin.dashboard.active = true
   lvim.builtin.alpha.mode = "startify"
