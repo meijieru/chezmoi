fbfbbe0f3e0b0408bcc8faa9f240f7091b49c3f3
meijieru <meijieru@gmail.com>
2021-11-17 16:48:09 -0500

    [nvim] disable keywords italic

--- dot_config/lvim/lua/core/defaults.lua
+++ dot_config/lvim/lua/core/defaults.lua
@@ -1,7 +1,7 @@
 _G.myvim = {
   root_markers = { ".git", ".root", ".project", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
 
-  colorscheme_enable_italic = true,
+  colorscheme_enable_italic = false,
   colorscheme_enable_italic_comment = true,
 
   plugins = {
