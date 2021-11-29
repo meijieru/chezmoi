a2e9e24d404204444f1efa9cb253b7302e480a25
meijieru <meijieru@gmail.com>
2021-11-28 22:33:17 -0500

    [nvim] dap python into ftplugin

---
+++ dot_config/lvim/after/ftplugin/python.lua
@@ -0,0 +1,45 @@
+local function dap_install_config()
+  if not require("core.utils").is_dap_debugger_installed "python" then
+    return
+  end
+
+  local opt = {
+    adapters = {
+      type = "executable",
+      command = "python3",
+      args = { "-m", "debugpy.adapter" },
+    },
+    configurations = {
+      {
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${workspaceFolder}/${file}",
+        pythonPath = function()
+          local venv_path = os.getenv "VIRTUAL_ENV"
+          if venv_path then
+            local util_sys = require "dap-install.utils.sys"
+            if util_sys.is_windows() then
+              return venv_path .. "\\Scripts\\python.exe"
+            end
+            return venv_path .. "/bin/python"
+          end
+
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+            return cwd .. "/venv/bin/python"
+          elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+            return cwd .. "/.venv/bin/python"
+          else
+            return "/usr/bin/python3"
+          end
+        end,
+      },
+    },
+  }
+
+  local dap_install = require "dap-install"
+  dap_install.config("python", opt)
+end
+
+dap_install_config()
--- dot_config/lvim/lua/core/utils.lua
+++ dot_config/lvim/lua/core/utils.lua
@@ -1,12 +1,12 @@
 local M = {}
 
+local Log = require "lvim.core.log"
+
 function M.load_vimscript(path)
   vim.cmd("source " .. vim.fn.expand "~/.config/lvim/" .. path)
 end
 
 function M.safe_load(name)
-  local Log = require "lvim.core.log"
-
   local status_ok, module = pcall(require, name)
   if not status_ok then
     Log:warn("Failed loading module: " .. name)
@@ -20,6 +20,26 @@ function M.map(tbl, f)
     t[k] = f(v)
   end
   return t
+end
+
+function M.is_dap_debugger_installed(name)
+  if not lvim.builtin.dap.active then
+    return false
+  end
+
+  local status, _ = M.safe_load "dap-install"
+  if not status then
+    Log:info "Plugin dap_install not installed"
+    return false
+  end
+
+  local dbg_list = require("dap-install.api.debuggers").get_installed_debuggers()
+  if not vim.tbl_contains(dbg_list, name) then
+    Log:info("Debugger " .. name .. " not installed")
+    return false
+  end
+
+  return true
 end
 
 return M
