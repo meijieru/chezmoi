3a6fbed0f09ca86c6816463ff9834ab03d8b11e0
meijieru <meijieru@gmail.com>
2021-11-29 05:46:47 -0500

    close #15

--- dot_config/lvim/lua/modules/lang/lvim.lua
+++ dot_config/lvim/lua/modules/lang/lvim.lua
@@ -6,6 +6,10 @@ function M.setup()
   lvim.lsp.null_ls.setup = {
     root_dir = require("modules.completion.lsp").root_dir,
   }
+
+  lvim.builtin.treesitter.highlight.is_supported = function(lang)
+    return require("nvim-treesitter.query").has_highlights(lang)
+  end
 end
 
 return M
