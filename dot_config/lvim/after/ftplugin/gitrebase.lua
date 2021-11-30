3b54b778881451bc496cb6dd0a48a9f6b7fe3f7b
meijieru <meijieru@gmail.com>
2021-11-30 18:30:18 -0500

    [nvim] code actions for gitsigns & gitrebase

---
+++ dot_config/lvim/after/ftplugin/gitrebase.lua
@@ -0,0 +1,2 @@
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup { { name = "gitrebase", filetypes = { "gitrebase" } } }
--- dot_config/lvim/lua/modules/lang/lvim.lua
+++ dot_config/lvim/lua/modules/lang/lvim.lua
@@ -1,15 +1,25 @@
 local M = {}
 
-function M.setup()
-  lvim.builtin.dap.active = true
-
+function M.setup_null_ls()
   lvim.lsp.null_ls.setup = {
     root_dir = require("modules.completion.lsp").root_dir,
   }
 
+  local code_actions = require "lvim.lsp.null-ls.code_actions"
+  code_actions.setup {
+    -- general sources, ft plugins check `after/ftplugin`
+    { name = "gitsigns" },
+  }
+end
+
+function M.setup()
+  lvim.builtin.dap.active = true
+
   lvim.builtin.treesitter.highlight.is_supported = function(lang)
     return require("nvim-treesitter.query").has_highlights(lang)
   end
+
+  M.setup_null_ls()
 end
 
 return M
