363c6677e3b9a17a0244108485b2f4196182705e
meijieru <meijieru@gmail.com>
2021-12-05 00:01:36 -0500

    [nvim] cmp no duplicate for buffer

---
+++ dot_config/lvim/lua/modules/completion/lvim.lua
@@ -0,0 +1,59 @@
+local M = {}
+
+function M.setup_cmp()
+  local cmp = require "cmp"
+
+  lvim.builtin.cmp.formatting.duplicates.buffer = 0
+  lvim.builtin.cmp.confirm_opts = {
+    behavior = cmp.ConfirmBehavior.Insert,
+    select = false,
+  }
+  lvim.builtin.cmp.formatting.kind_icons = {
+    Text = "",
+    Method = "",
+    Function = "",
+    Constructor = "",
+    Field = "ﰠ",
+    Variable = "",
+    Class = "ﴯ",
+    Interface = "",
+    Module = "",
+    Property = "ﰠ",
+    Unit = "塞",
+    Value = "",
+    Enum = "",
+    Keyword = "",
+    Snippet = "",
+    Color = "",
+    File = "",
+    Reference = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "פּ",
+    Event = "",
+    Operator = "",
+    TypeParameter = "",
+  }
+end
+
+function M.setup_trouble()
+  if not lvim.builtin.telescope.active then
+    return
+  end
+  local function _open_with_trouble(prompt_bufnr, _mode)
+    -- defer trouble loading
+    local trouble = require "trouble.providers.telescope"
+    return trouble.open_with_trouble(prompt_bufnr, _mode)
+  end
+
+  lvim.builtin.telescope.defaults.mappings.i["<C-t>"] = _open_with_trouble
+  lvim.builtin.telescope.defaults.mappings.n["<C-t>"] = _open_with_trouble
+end
+
+function M.setup()
+  M.setup_trouble()
+  M.setup_cmp()
+end
+
+return M
