7a4c9b7598eefa566e3a1e2ac1c2a3fb646a78d3
meijieru <meijieru@gmail.com>
2021-12-07 14:59:32 -0500

    [nvim] feat(log-highlighting): lazy load

--- dot_config/lvim/lua/modules/lang/plugins.lua
+++ dot_config/lvim/lua/modules/lang/plugins.lua
@@ -14,6 +14,6 @@ lang["iamcco/markdown-preview.nvim"] = {
 lang["jbyuki/one-small-step-for-vimkind"] = { ft = { "lua" } }
 lang["lervag/vimtex"] = { ft = { "tex" }, disable = true }
 lang["teal-language/vim-teal"] = { ft = { "teal" } }
-lang["MTDL9/vim-log-highlighting"] = {}
+lang["MTDL9/vim-log-highlighting"] = { ft = { "log" } }
 
 return lang
