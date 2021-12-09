bd01c032fbe77fc99bad7e0c6c49c4c93a6ea82a
meijieru <meijieru@gmail.com>
2021-12-08 21:33:06 -0500

    [nvim] feat(telescope): checktime for git_checkout

--- dot_config/lvim/lua/modules/tools/lvim.lua
+++ dot_config/lvim/lua/modules/tools/lvim.lua
@@ -1,7 +1,26 @@
 local M = {}
 
-function M.setup()
+function M.setup_telescope()
+  lvim.builtin.telescope.defaults.mappings.i["<C-?>"] = "which_key"
+  lvim.builtin.telescope.defaults.mappings.n["g?"] = "which_key"
   lvim.builtin.telescope.defaults.layout_config.width = 0.95
+  lvim.builtin.telescope.on_config_done = function()
+    local actions = require "telescope.actions"
+    local function checktime_wrapper(func_name)
+      local func = actions[func_name]
+      return function(...)
+        func(...)
+        vim.cmd "checktime"
+      end
+    end
+    -- checktime to refresh the content
+    actions.git_checkout = checktime_wrapper "git_checkout"
+    actions.git_checkout_current_buffer = checktime_wrapper "git_checkout_current_buffer"
+  end
+end
+
+function M.setup()
+  M.setup_telescope()
 end
 
 return M
