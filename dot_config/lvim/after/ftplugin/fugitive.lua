acb4a8e2e6a387fdb092f19e818b29c8e46d5f67
meijieru <meijieru@gmail.com>
2021-12-09 04:04:16 -0500

    [nvim] fix(fugitive): dt keymap, fix #22

--- dot_config/lvim/after/ftplugin/fugitive.lua
+++ dot_config/lvim/after/ftplugin/fugitive.lua
@@ -1,9 +1,10 @@
 local function keymap_config()
   local mapx = require "mapx"
+  local keymap = require "core.keymap"
 
   mapx.nnoremap("q", "<cmd>close<cr>", mapx.silent, mapx.buffer)
   -- https://github.com/tpope/vim-fugitive/issues/1451#issuecomment-770310789
-  mapx.nmap("dt", ":Gtabedit <Plug><cfile><Bar>Gdiffsplit<CR>", mapx.buffer)
+  mapx.nnoremap("dt", keymap.chain("Gtabedit", "normal dv"), "Tab Diff")
 end
 
 keymap_config()
