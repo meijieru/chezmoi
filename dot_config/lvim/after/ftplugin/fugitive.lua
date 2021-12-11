5eb6d6960c2df8756f49aef41c985a8da2e264e1
meijieru <meijieru@gmail.com>
2021-12-10 23:22:30 -0500

    [nvim] fix(keymap): dt only on current fugitive

--- dot_config/lvim/after/ftplugin/fugitive.lua
+++ dot_config/lvim/after/ftplugin/fugitive.lua
@@ -2,9 +2,9 @@ local function keymap_config()
   local mapx = require "mapx"
   local keymap = require "core.keymap"
 
-  mapx.nnoremap("q", "<cmd>close<cr>", mapx.silent, mapx.buffer)
+  mapx.nnoremap("q", "<cmd>close<cr>", mapx.silent, mapx.buffer, "Close")
   -- https://github.com/tpope/vim-fugitive/issues/1451#issuecomment-770310789
-  mapx.nnoremap("dt", keymap.chain("Gtabedit", "normal dv"), "Tab Diff")
+  mapx.nnoremap("dt", keymap.chain("Gtabedit", "normal dv"), mapx.buffer, "Tab Diff")
 end
 
 keymap_config()
