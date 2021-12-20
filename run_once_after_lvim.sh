d8de5d1e5a518885ecf78b82b15f1df80194c209
meijieru <meijieru@gmail.com>
2021-12-19 21:17:25 -0500

    [nvim] feat(lvim): install

--- run_once_after_lvim.sh
+++ run_once_after_lvim.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-git clone ssh://git@direct.meijieru.com:2222/meijieru/LunarVim.git ~/lib/LunarVim
+export LUNAR_PATH=~/lib/LunarVim
+git clone ssh://git@direct.meijieru.com:2222/meijieru/LunarVim.git ${LUNAR_PATH}
+cd ${LUNAR_PATH}
+make install
