bf79873b27423f71196652333a2d9e5354532abd
meijieru <meijieru@gmail.com>
2022-01-01 13:19:23 -0500

    [nvim] fix(lang): disable several lang plugins

--- dot_config/lvim/lua/modules/lang/config.lua
+++ dot_config/lvim/lua/modules/lang/config.lua
@@ -4,18 +4,22 @@ local config = {}
 -- $ANACONDA_HOME should be set in shell
 local anaconda_home = os.getenv "ANACONDA_HOME"
 if anaconda_home ~= nil then
-  vim.g.python_host_prog = anaconda_home .. "/bin/python2"
   vim.g.python3_host_prog = anaconda_home .. "/bin/python3"
 else
-  vim.g.python_host_prog = "/usr/bin/python2"
   vim.g.python3_host_prog = "/usr/bin/python3"
 end
+
+-- disable languages
+vim.g.loaded_python_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
 
 require("modules.lang.lvim").setup()
 
 function config.markdown_preview()
   vim.g.mkdp_auto_start = 0
-  vim.g.mkdp_browser='wsl-open'
+  vim.g.mkdp_browser = "wsl-open"
 end
 
 return config
