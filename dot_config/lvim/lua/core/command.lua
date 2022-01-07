eb930a17a8fa22828517991123435a8274e0f0d1
meijieru <meijieru@gmail.com>
2022-01-07 17:00:07 -0500

    [nvim] feat(dap): `MyDapLoadBreakpoints` & `MyDapStoreBreakpoints`

--- dot_config/lvim/lua/core/command.lua
+++ dot_config/lvim/lua/core/command.lua
@@ -1,14 +1,9 @@
 local Log = require "core.log"
 if vim.api.nvim_add_user_command == nil then
-  Log:debug "Skip command define"
+  Log:info "Skip my command define"
   return
 end
 
-vim.api.nvim_add_user_command("MyInspect", function(kwargs)
-  local vars = vim.split(kwargs.args, ".", { plain = true, trimempty = true })
-  local var = _G
-  for _, key in ipairs(vars) do
-    var = var[key]
-  end
-  print(vim.inspect { var })
-end, { desc = "Lua Inspect", nargs = "+" })
+local dap_utils = require "core.utils.dap"
+vim.api.nvim_add_user_command("MyDapStoreBreakpoints", dap_utils.store, { desc = "Save DAP breakpoints" })
+vim.api.nvim_add_user_command("MyDapLoadBreakpoints", dap_utils.load, { desc = "Load DAP breakpoints" })
