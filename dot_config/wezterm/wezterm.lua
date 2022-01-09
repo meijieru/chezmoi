bc547b7490f41b1377d8d31a542ffbd780fc089f
meijieru <meijieru@gmail.com>
2022-01-09 15:02:39 -0500

    [wezterm] disable ligature for `victor mono` only, close #30

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -43,17 +43,33 @@ return {
     {
       italic = true,
       intensity = "Bold",
-      font = font_with_fallback({ "Victor Mono" }, { weight = "Bold", italic = true }),
+      font = font_with_fallback({
+        {
+          family = "Victor Mono",
+          -- disable ligature due to mixed fonts, it cause uneven fonts
+          harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+        },
+      }, {
+        weight = "Bold",
+        italic = true,
+      }),
     },
     {
       intensity = "Bold",
       font = font_with_fallback({ "JetBrains Mono" }, { weight = "Bold" }),
-      -- font = font_with_fallback({ "JetBrains Mono", "Microsoft Yahei" }, { weight = "Bold" }),
     },
     {
       italic = true,
-      font = font_with_fallback({ "Victor Mono" }, { weight = "Medium", italic = true }),
-      -- font = font_with_fallback("Victor Mono", { weight = "Semibold", italic = true }),
+      font = font_with_fallback({
+        {
+          family = "Victor Mono",
+          harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+        },
+      }, {
+        weight = "Medium",
+        -- weight = "Semibold",
+        italic = true,
+      }),
     },
     {
       intensity = "Normal",
@@ -69,7 +85,7 @@ return {
   }),
   font_size = 13.0,
   line_height = 0.95,
-  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  -- harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 
   default_prog = { "wsl.exe", "~", "-d", "archlinux" },
 
@@ -78,7 +94,7 @@ return {
   -- window_decorations = "NONE",
   window_padding = { left = 5, right = 0, top = 0, bottom = 0 },
 
-  window_background_image = "D:\\document\\nextcloud\\picture\\comic\\eva\\EVA_OneHalfLight.jpg",
+  window_background_image = "D:\\document\\cloud_storage\\nextcloud\\picture\\comic\\eva\\EVA_OneHalfLight.jpg",
   window_background_image_hsb = {
     brightness = 1.0,
     hue = 1.0,
