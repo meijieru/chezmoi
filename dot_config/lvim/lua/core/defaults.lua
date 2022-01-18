900d48a9073b5a61c013376a93afb5297cafcd71
meijieru <meijieru@gmail.com>
2022-01-18 06:42:46 +0000

    [nvim] fix(project): `BUILD` as root marker

--- dot_config/lvim/lua/core/defaults.lua
+++ dot_config/lvim/lua/core/defaults.lua
@@ -4,7 +4,7 @@ _G.myvim = {
     override_notify = true,
   },
   colorscheme = "edge_lush",
-  root_markers = { ".git", ".root", ".project", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+  root_markers = { ".git", ".root", ".project", "BUILD", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
 
   ignores = {
     buftype = { "quickfix", "nofile", "help", "terminal" },
