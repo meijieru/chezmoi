2c1887e5dccf748848a29128523a5b438392071d
meijieru <meijieru@gmail.com>
2022-05-29 21:33:58 -0700

    [nvim] fix(autocmd): use lua api

--- dot_config/lvim/lua/core/autocmd.lua
+++ dot_config/lvim/lua/core/autocmd.lua
@@ -1,9 +1,15 @@
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 
-lvim.autocommands._markdown = nil
-lvim.autocommands.custom_groups = {
-  -- https://www.chezmoi.io/docs/how-to/#configure-vim-to-run-chezmoi-apply-whenever-you-save-a-dotfile
-  { "BufWritePost", "~/.local/share/chezmoi/*", "lua require('core.utils').chezmoi_apply()" },
-  -- disable fold
-  { "FileType", "alpha,lspinfo,aerial,dapui_scopes", "setlocal nofoldenable" },
-}
+local create_autocmd = vim.api.nvim_create_autocmd
+
+-- https://www.chezmoi.io/docs/how-to/#configure-vim-to-run-chezmoi-apply-whenever-you-save-a-dotfile
+create_autocmd(
+  "BufWritePost",
+  { pattern = { "*/.local/share/chezmoi/*" }, command = "lua require('core.utils').chezmoi_apply()" }
+)
+
+-- disable fold
+create_autocmd(
+  "FileType",
+  { pattern = { "alpha", "lspinfo", "aerial", "dapui_scopes" }, command = "setlocal nofoldenable" }
+)
