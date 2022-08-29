2d2a23d29291c3d97c028ff293f5b4c0f462f433
meijieru <meijieru@gmail.com>
2022-08-29 15:35:08 -0700

    [nvim] chore(filetype): minor clean

--- dot_config/lvim/ftdetect/base.lua
+++ dot_config/lvim/ftdetect/base.lua
@@ -1,13 +1,12 @@
 local Log = require "core.log"
+
 if vim.filetype == nil then
   Log:info "Skip customized filetypes"
   return
 end
 
 vim.filetype.add {
-  extension = {
-    tl = "teal",
-  },
+  extension = {},
   filename = {
     [".tasks"] = "dosini",
   },
