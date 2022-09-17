2ca342c433b2003f67d2cd47c65778fff0ffc054
meijieru <meijieru@gmail.com>
2022-09-17 01:09:15 -0700

    [nvim] chore(autocmd): better description

--- dot_config/lvim/lua/core/autocmd.lua
+++ dot_config/lvim/lua/core/autocmd.lua
@@ -3,10 +3,13 @@
 local create_autocmd = vim.api.nvim_create_autocmd
 
 -- https://www.chezmoi.io/docs/how-to/#configure-vim-to-run-chezmoi-apply-whenever-you-save-a-dotfile
-create_autocmd(
-  "BufWritePost",
-  { pattern = { "*/.local/share/chezmoi/*" }, command = "lua require('core.utils').chezmoi_apply()" }
-)
+create_autocmd("BufWritePost", {
+  pattern = { "*/.local/share/chezmoi/*" },
+  callback = function()
+    require("core.utils").chezmoi_apply()
+  end,
+  desc="Trigger chezmoi apply"
+})
 
 -- disable fold
 create_autocmd(
