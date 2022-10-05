1f0b075961bbfa61079147495205b7ebcd0ae1be
meijieru <meijieru@gmail.com>
2022-10-05 13:38:08 -0700

    [nvim] fix(oscyank): silent copy

--- dot_config/lvim/lua/modules/editor/config.lua
+++ dot_config/lvim/lua/modules/editor/config.lua
@@ -87,6 +87,7 @@ function M.neogen()
 end
 
 function M.oscyank()
+  vim.g.oscyank_silent = true
   vim.cmd [[
     " autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '+' | execute 'OSCYankReg +' | endif
     autocmd TextYankPost * if v:event.operator is 'y' && v:event.regname is '' | execute 'OSCYankReg "' | endif
