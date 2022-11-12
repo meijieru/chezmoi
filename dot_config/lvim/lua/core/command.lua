322cf79a827ee52028e5e9e92e67277ca0781fb4
meijieru <meijieru@gmail.com>
2022-11-12 03:26:54 -0800

    [nvim] fix(editor): update to latest api

--- dot_config/lvim/lua/core/command.lua
+++ dot_config/lvim/lua/core/command.lua
@@ -1,9 +1,9 @@
 local Log = require "core.log"
-if vim.api.nvim_add_user_command == nil then
+if vim.api.nvim_create_user_command == nil then
   Log:info "Skip customized commands"
   return
 end
 
 local dap_utils = require "core.utils.dap"
-vim.api.nvim_add_user_command("MyDapStoreBreakpoints", dap_utils.store, { desc = "Save DAP breakpoints" })
-vim.api.nvim_add_user_command("MyDapLoadBreakpoints", dap_utils.load, { desc = "Load DAP breakpoints" })
+vim.api.nvim_create_user_command("MyDapStoreBreakpoints", dap_utils.store, { desc = "Save DAP breakpoints" })
+vim.api.nvim_create_user_command("MyDapLoadBreakpoints", dap_utils.load, { desc = "Load DAP breakpoints" })
