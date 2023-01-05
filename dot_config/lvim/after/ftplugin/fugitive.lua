8abdef49fd6e36cb1c0732a3ff4b3ae3d9c64e58
meijieru <meijieru@gmail.com>
2023-01-04 16:14:30 -0800

    [nvim] fix(fugitive): `dt` keymap leakage

--- dot_config/lvim/after/ftplugin/fugitive.lua
+++ dot_config/lvim/after/ftplugin/fugitive.lua
@@ -1,10 +1,9 @@
 local function keymap_config()
-  local mapx = require "mapx"
   local keymap = require "core.keymap"
 
-  mapx.nnoremap("q", "<cmd>close<cr>", mapx.silent, mapx.buffer, "Close")
+  vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = true, desc = "Close" })
   -- https://github.com/tpope/vim-fugitive/issues/1451#issuecomment-770310789
-  mapx.nnoremap("dt", keymap.chain("Gtabedit", "normal dv"), mapx.buffer, "Tab Diff")
+  vim.keymap.set("n", "dt", keymap.chain("Gtabedit", "normal dv"), { buffer = true, desc = "Tab Diff" })
 end
 
 keymap_config()
