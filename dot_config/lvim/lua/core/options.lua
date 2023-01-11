a210fd2d1bda66eee84c511892874db9a724c3ad
meijieru <meijieru@gmail.com>
2023-01-11 00:58:40 -0800

    [nvim] chore(misc): refine options

--- dot_config/lvim/lua/core/options.lua
+++ dot_config/lvim/lua/core/options.lua
@@ -1,29 +1,23 @@
-vim.o.ruler = false
 -- vim.o.cmdheight = 0
+vim.o.foldcolumn = "1"
 if myvim.plugins.ufo.active then
-  vim.o.foldcolumn = "1"
   vim.o.foldlevel = 99
   vim.o.foldlevelstart = -1
-  vim.o.foldenable = true
   vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 else
   vim.o.foldmethod = "expr"
   vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 end
--- vim.o.mouse = ""
 vim.o.showtabline = 1
-vim.o.splitbelow = false
-vim.o.splitright = false
 vim.o.timeoutlen = 500
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.showbreak = "↳ "
-vim.o.diffopt = "filler,iwhite,internal,algorithm:patience" -- use patience diff algorithm
-vim.o.colorcolumn = ""
+vim.opt.diffopt:append { "internal", "algorithm:patience", "indent-heuristic" }
 vim.o.grepprg = [[rg --hidden --glob "!.git" --no-heading --smart-case --vimgrep --follow $*]]
 vim.o.grepformat = "%f:%l:%c:%m"
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
 vim.o.laststatus = 3
+vim.o.background = "light"
 
 if vim.fn.has "nvim-0.9" then
   vim.o.splitkeep = "screen"
@@ -37,6 +31,4 @@ vim.o.qftf = "{info -> v:lua._G.qftf(info, 'shorten')}"
 lvim.log = vim.tbl_deep_extend("force", lvim.log, myvim.log)
 lvim.format_on_save = false
 lvim.leader = "space"
-vim.o.background = "light"
 lvim.colorscheme = myvim.colorscheme.name
--- lvim.transparent_window = true
