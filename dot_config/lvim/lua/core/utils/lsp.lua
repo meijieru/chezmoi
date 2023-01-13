ec7a0ab686af72e9e3cdbc2fa576d67beb2d754c
meijieru <meijieru@gmail.com>
2023-01-13 14:22:55 -0800

    [nvim] chore(lsp): improve toggle_diagnostics

--- dot_config/lvim/lua/core/utils/lsp.lua
+++ dot_config/lvim/lua/core/utils/lsp.lua
@@ -1,18 +1,22 @@
-local M = {
-  _diagnostics_active = true,
-}
+local M = {}
+
+local Log = require "core.log"
 
 function M.root_dir(fname)
   return require("lspconfig.util").root_pattern(unpack(myvim.root_markers))(fname) or vim.loop.cwd()
 end
 
-function M.toggle_diagnostics()
-  if M._diagnostics_active then
-    vim.diagnostic.disable(0)
-    M._diagnostics_active = false
+--- Toggle diagnostics
+--- @param bufnr number | nil
+--- @param namespace number | nil
+function M.toggle_diagnostics(bufnr, namespace)
+  if vim.diagnostic.is_disabled == nil then
+    Log:warn "toggle_diagnostics skipped"
+  end
+  if vim.diagnostic.is_disabled(bufnr, namespace) then
+    vim.diagnostic.enable(bufnr, namespace)
   else
-    vim.diagnostic.enable(0)
-    M._diagnostics_active = true
+    vim.diagnostic.disable(bufnr, namespace)
   end
 end
 
