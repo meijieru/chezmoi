d35a2cb11e085e3c717a2d36073246a068f715da
meijieru <meijieru@gmail.com>
2023-01-13 14:04:14 -0800

    [nvim] chore(lsp): disable gitsigns actions

--- dot_config/lvim/lua/modules/lang/lvim.lua
+++ dot_config/lvim/lua/modules/lang/lvim.lua
@@ -4,16 +4,6 @@ function M.setup_null_ls()
   lvim.lsp.null_ls.setup = {
     root_dir = require("core.utils.lsp").root_dir,
   }
-
-  local code_actions = require "lvim.lsp.null-ls.code_actions"
-  code_actions.setup {
-    -- general sources, ft plugins check `after/ftplugin`
-    {
-      name = "gitsigns",
-      -- TODO(meijieru): wait for https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428
-      disabled_filetypes = { "cpp", "proto", "cuda" },
-    },
-  }
 end
 
 function M.setup()
