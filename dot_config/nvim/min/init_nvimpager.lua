66b1d5e6e5055350b51211c742a644c602cfacaf
meijieru <meijieru@gmail.com>
2023-01-15 14:40:07 -0800

    fix(nvimpager): use `lazy.nvim`

--- dot_config/nvim/min/init_nvimpager.lua
+++ dot_config/nvim/min/init_nvimpager.lua
@@ -5,84 +5,88 @@ local function join_paths(...)
   return result
 end
 
-local function load_pack(dir)
-  vim.cmd.packadd(dir)
-end
-
 local HOME = os.getenv("HOME")
 local XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or join_paths(HOME, ".config")
 local XDG_DATA_HOME = os.getenv("XDG_DATA_HOME") or join_paths(HOME, ".local/share")
-local base_dir = join_paths(XDG_DATA_HOME, "lunarvim/site")
-local package_root = base_dir .. "/pack"
-local nvimpager_dir = join_paths(XDG_CONFIG_HOME, "nvimpager")
-local compile_path = join_paths(nvimpager_dir, "plugin", "packer_compiled.lua")
-local runtimes = { base_dir, nvimpager_dir }
+local runtime_dir = join_paths(XDG_DATA_HOME, "nvimpager")
+local config_dir = join_paths(XDG_CONFIG_HOME, "nvimpager")
+-- Reuse lvim lazy.nvim
+local lazy_dir = join_paths(XDG_DATA_HOME, "lunarvim", "site", "pack", "lazy", "opt", "lazy.nvim")
+local plugins_dir = join_paths(runtime_dir, "site", "pack", "lazy", "opt")
+local runtimes = { config_dir, lazy_dir }
 for _, dir in ipairs(runtimes) do
   if not vim.tbl_contains(vim.opt.rtp:get(), dir) then
     vim.opt.rtp:append(dir)
   end
 end
 
-vim.o.packpath = vim.o.runtimepath
-vim.cmd.packadd("packer.nvim")
-
-local _, packer = pcall(require, "packer")
-packer.startup({
-  function(use)
-    use({
-      "meijieru/edge.nvim",
-      opt = true,
-      requires = { "rktjmp/lush.nvim", opt = true },
-    })
-    use({ "sainnhe/edge", opt = true })
-    use({
-      "nvim-treesitter/nvim-treesitter",
-      config = function()
-        require("nvim-treesitter.configs").setup({
-          highlight = { enable = true, additional_vim_regex_highlighting = false },
-        })
-      end,
-    })
-    use({ "bronson/vim-visual-star-search" })
-    use({
-      "ggandor/leap.nvim",
-      config = function()
-        require("leap").add_default_mappings()
-      end,
-    })
-  end,
-  config = {
-    package_root = package_root,
-    compile_path = compile_path,
-    log = { level = "warn" },
-  },
-})
-if vim.fn.filereadable(compile_path) == 0 then
-  packer.compile()
+local lazy_available, lazy = pcall(require, "lazy")
+if not lazy_available then
+  vim.notify("skipping loading plugins until lazy.nvim is installed", "warn")
+  return
 end
 
-vim.o.clipboard = "unnamedplus"
+local colorscheme = "everforest"
+
+local plugins = {
+  { "sainnhe/everforest", lazy = false },
+  {
+    "ibhagwan/smartyank.nvim",
+    event = { "VeryLazy" },
+    opts = {
+      highlight = {
+        higroup = "Search",
+        timeout = 200,
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        highlight = { enable = true, additional_vim_regex_highlighting = false },
+      })
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    event = { "VeryLazy" },
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+}
+local opts = {
+  install = {
+    missing = true,
+    colorscheme = { colorscheme },
+  },
+  ui = {
+    border = "rounded",
+  },
+  root = plugins_dir,
+  lockfile = join_paths(config_dir, "lazy-lock.json"),
+  performance = {
+    rtp = {
+      reset = false,
+    },
+  },
+  readme = {
+    root = join_paths(runtime_dir, "lazy", "readme"),
+  },
+}
+
+lazy.setup(plugins, opts)
+
 vim.o.background = "light"
+vim.o.termguicolors = true
 vim.g.mapleader = " "
+
 -- imcompatible with nvimpager's setting
 -- vim.o.scrolloff = 8
 -- vim.o.sidescrolloff = 8
 
-load_pack("lush.nvim")
-load_pack("edge.nvim")
-vim.cmd.colorscheme("edge_lush")
+vim.cmd.colorscheme(colorscheme)
 
 -- prevent load tpipeline
 vim.g.loaded_tpipeline = 1
-
---- keymap
-
---- autocmd
-local gid = vim.api.nvim_create_augroup("lua_highlight", {})
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = gid,
-  pattern = { "*" },
-  callback = function()
-    require("vim.highlight").on_yank({ higroup = "Search", timeout = 200 })
-  end,
-})
