2b6b8a6dbe3303e880501fee0436ee29faf93f29
meijieru <meijieru@gmail.com>
2023-01-19 17:50:10 -0800

    feat(nvim): always set `LVIM_DEV_MODE`

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,6 +3,7 @@ local envs = {
   LUNARVIM_RUNTIME_DIR = home_dir .. "/.local/share/lunarvim",
   LUNARVIM_CONFIG_DIR = home_dir .. "/.config/lvim",
   LUNARVIM_CACHE_DIR = home_dir .. "/.cache/lvim",
+  LVIM_DEV_MODE = 1,
 }
 for key, val in pairs(envs) do
   vim.loop.os_setenv(key, val)
