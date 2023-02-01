0567189440839fb0e2056072a294b23dd2a55255
meijieru <meijieru@gmail.com>
2023-01-31 19:03:01 -0500

    feat(nvim): command `UpdateAll`

--- dot_config/lvim/lua/core/command.lua
+++ dot_config/lvim/lua/core/command.lua
@@ -7,3 +7,10 @@ end
 local dap_utils = require "core.utils.dap"
 vim.api.nvim_create_user_command("MyDapStoreBreakpoints", dap_utils.store, { desc = "Save DAP breakpoints" })
 vim.api.nvim_create_user_command("MyDapLoadBreakpoints", dap_utils.load, { desc = "Load DAP breakpoints" })
+
+vim.api.nvim_create_user_command("UpdateAll", function()
+  vim.cmd "TSUpdate"
+  require("lazy").sync { show = true }
+end, {
+  desc = "Update plugins & lsp & treesitter parsers",
+})
