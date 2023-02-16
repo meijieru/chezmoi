c318ea9bcbfdaac581afcaf04bad9b08e4bd530f
meijieru <meijieru@gmail.com>
2023-02-16 09:19:55 -0500

    feat(nvim): disable internal black plugin

--- dot_config/lvim/lua/core/init.lua
+++ dot_config/lvim/lua/core/init.lua
@@ -19,6 +19,7 @@ function M.setup_distribution_plugins()
   vim.g.loaded_netrwPlugin = 1
   vim.g.loaded_netrwSettings = 1
   vim.g.loaded_netrwFileHandlers = 1
+  vim.g.load_black = 1
 end
 
 function M.load_plugins()
