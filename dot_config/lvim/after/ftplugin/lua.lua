15c69906c046949f851cf7040b4fa467b43d7ca0
meijieru <meijieru@gmail.com>
2023-02-17 19:48:50 -0500

    fix(nvim): remove lua hover wrong highlight

--- dot_config/lvim/after/ftplugin/lua.lua
+++ dot_config/lvim/after/ftplugin/lua.lua
@@ -2,3 +2,8 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { exe = "stylua" } }
 -- local linters = require "lvim.lsp.null-ls.linters"
 -- linters.setup { { exe = "luacheck" } }
+
+-- TODO(meijieru): currently the hover has wrong highlight
+for _, hl in ipairs { "luaParenError", "luaError", "markdownError" } do
+  vim.api.nvim_set_hl(0, hl, { link = "None" })
+end
