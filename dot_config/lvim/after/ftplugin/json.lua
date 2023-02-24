57399adae9f6e163c9bf0a17adbc6a4a1d9cac2b
meijieru <meijieru@gmail.com>
2023-02-24 04:18:55 -0500

    fix(nvim): json tab setting leak

--- dot_config/lvim/after/ftplugin/json.lua
+++ dot_config/lvim/after/ftplugin/json.lua
@@ -1,5 +1,8 @@
-vim.o.tabstop = 2
-vim.o.shiftwidth = 2
+local Log = require "core.log"
+
+Log:debug "set tabstop from filetype plugin"
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { exe = "prettier", filetypes = { "json" } } }
