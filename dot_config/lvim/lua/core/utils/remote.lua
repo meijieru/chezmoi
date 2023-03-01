eb9018d154d967cc89b5b648764b5e47409797b3
meijieru <meijieru@gmail.com>
2023-03-01 01:15:17 -0500

    fix(nvim): find remote config recursively with cache

--- dot_config/lvim/lua/core/utils/remote.lua
+++ dot_config/lvim/lua/core/utils/remote.lua
@@ -1,8 +1,24 @@
 local M = {}
 
+---@class remote.Cache
+---@field config_path table
+---@field config table
+
+---@type remote.Cache
+local _cache
+
+function M.reset_cache()
+  _cache = {
+    config_path = {},
+    config = {},
+  }
+end
+M.reset_cache()
+
 local utils = require "core.utils"
 local Log = require "core.log"
-local config_name = ".vim/remote.json"
+local config_dir_name = ".vim"
+local config_name = "remote.json"
 
 ---@class remote.RemoteConfig
 ---@field hosts string[]
@@ -13,30 +29,37 @@ local config_name = ".vim/remote.json"
 ---@return string?
 function M.get_remote_config_path()
   local cwd = vim.loop.cwd()
-  local file = join_paths(cwd, config_name)
+  local cache = _cache.config_path
+  if cache[cwd] ~= nil then
+    Log:debug(string.format("use cached remote config path: %s -> %s", cwd, cache[cwd]))
+    return cache[cwd]
+  end
+
+  local config_dir = vim.fs.find(config_dir_name, { upward = true })[1]
+  if config_dir == nil then
+    return
+  end
+  local file = join_paths(config_dir, config_name)
   if vim.fn.filereadable(file) == 1 then
+    Log:debug(string.format("find remote config file: %s -> %s", cwd, file))
+    cache[cwd] = file
     return file
   end
-end
-
-local _remote_config_cache = {}
-
-function M.reset_cache()
-  _remote_config_cache = {}
 end
 
 --- Get the remote config, use cached value if available
 ---@return remote.RemoteConfig?
 function M.get_remote_config()
   local remote_config_path = M.get_remote_config_path()
+  local cache = _cache.config
   if remote_config_path ~= nil then
-    if vim.tbl_contains(_remote_config_cache, remote_config_path) then
-      Log:debug "use cached remote config"
-      return _remote_config_cache[remote_config_path]
+    if cache[remote_config_path] ~= nil then
+      Log:debug(string.format("use cached remote config: %s", remote_config_path))
+      return cache[remote_config_path]
     end
     Log:debug "read remote config"
     local config = utils.read_json(remote_config_path)
-    _remote_config_cache[remote_config_path] = config
+    cache[remote_config_path] = config
     return config
   end
 end
