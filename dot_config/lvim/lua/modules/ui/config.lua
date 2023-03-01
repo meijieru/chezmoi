9a5f608a0b157c353b5fd741bdf5cc1821e7aaa8
meijieru <meijieru@gmail.com>
2023-02-28 23:34:53 -0500

    chore(nvim): statuscol config breaking update

--- dot_config/lvim/lua/modules/ui/config.lua
+++ dot_config/lvim/lua/modules/ui/config.lua
@@ -112,10 +112,18 @@ function M.oil()
 end
 
 function M.statuscol()
+  local builtin = require "statuscol.builtin"
   require("statuscol").setup {
     setopt = true,
-    order = "NSFs",
-    foldfunc = "builtin",
+    segments = {
+      {
+        text = { builtin.lnumfunc, " " },
+        condition = { true, builtin.not_empty },
+        click = "v:lua.ScLa",
+      },
+      { text = { "%s" }, click = "v:lua.ScSa" },
+      { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+    },
   }
 end
 
