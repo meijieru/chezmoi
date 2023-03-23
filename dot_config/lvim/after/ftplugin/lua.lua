337f1aa0e4569b4dfd619b41c1ef9bd156f986b6
meijieru <meijieru@gmail.com>
2023-03-23 15:55:51 -0700

    chore(nvim): lua indent 2

--- dot_config/lvim/after/ftplugin/lua.lua
+++ dot_config/lvim/after/ftplugin/lua.lua
@@ -1,3 +1,9 @@
+local Log = require "core.log"
+
+Log:debug "set tabstop from filetype plugin"
+vim.bo.tabstop = 2
+vim.bo.shiftwidth = 2
+
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { exe = "stylua" } }
 -- local linters = require "lvim.lsp.null-ls.linters"
