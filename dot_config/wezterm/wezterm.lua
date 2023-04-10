8f2fbe00d7e51a82cb8fd86dad863fe9ad3bbec4
meijieru <meijieru@gmail.com>
2023-04-10 05:33:13 -0400

    chore(nvim): use webgpu backend

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -9,19 +9,6 @@ local function scheme_for_appearance(appearance)
   end
 end
 
-local mykeys = {
-  { key = "{", mods = "SHIFT|ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
-  { key = "}", mods = "SHIFT|ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
-}
-for i = 1, 8 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(mykeys, {
-    key = tostring(i),
-    mods = "CTRL|ALT",
-    action = wezterm.action({ ActivateTab = i - 1 }),
-  })
-end
-
 local function font_with_fallback(fonts, params)
   if #fonts ~= 1 then
     error("Only support one")
@@ -42,48 +29,59 @@ wezterm.on("gui-startup", function(cmd)
 end)
 
 local normal_font = "JetBrains Mono"
+local font_rules = {
+  {
+    italic = true,
+    intensity = "Bold",
+    font = font_with_fallback({
+      {
+        family = "Victor Mono",
+        -- disable ligature due to mixed fonts, it cause uneven fonts
+        harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+      },
+    }, {
+      weight = "Bold",
+      italic = true,
+    }),
+  },
+  {
+    intensity = "Bold",
+    font = font_with_fallback({ normal_font }, { weight = "Bold" }),
+  },
+  {
+    italic = true,
+    font = font_with_fallback({
+      {
+        family = "Victor Mono",
+        harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+      },
+    }, {
+      weight = "Medium",
+      -- weight = "Semibold",
+      italic = true,
+    }),
+  },
+  {
+    intensity = "Normal",
+    font = font_with_fallback({ normal_font }, { weight = "Regular" }),
+  },
+}
+
+local mykeys = {
+  { key = "{", mods = "SHIFT|ALT", action = wezterm.action({ ActivateTabRelative = -1 }) },
+  { key = "}", mods = "SHIFT|ALT", action = wezterm.action({ ActivateTabRelative = 1 }) },
+}
+for i = 1, 8 do
+  -- CTRL+ALT + number to activate that tab
+  table.insert(mykeys, {
+    key = tostring(i),
+    mods = "CTRL|ALT",
+    action = wezterm.action({ ActivateTab = i - 1 }),
+  })
+end
 
 return {
-  hide_tab_bar_if_only_one_tab = false,
-  enable_tab_bar = true,
-
-  font_rules = {
-    {
-      italic = true,
-      intensity = "Bold",
-      font = font_with_fallback({
-        {
-          family = "Victor Mono",
-          -- disable ligature due to mixed fonts, it cause uneven fonts
-          harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-        },
-      }, {
-        weight = "Bold",
-        italic = true,
-      }),
-    },
-    {
-      intensity = "Bold",
-      font = font_with_fallback({ normal_font }, { weight = "Bold" }),
-    },
-    {
-      italic = true,
-      font = font_with_fallback({
-        {
-          family = "Victor Mono",
-          harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-        },
-      }, {
-        weight = "Medium",
-        -- weight = "Semibold",
-        italic = true,
-      }),
-    },
-    {
-      intensity = "Normal",
-      font = font_with_fallback({ normal_font }, { weight = "Regular" }),
-    },
-  },
+  font_rules = font_rules,
   font = font_with_fallback({
     normal_font,
   }),
@@ -95,19 +93,12 @@ return {
 
   color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 
-  -- window_decorations = "NONE",
-  window_padding = { left = 5, right = 0, top = 0, bottom = 0 },
-
-  window_background_image = "D:\\document\\cloud_storage\\nextcloud\\picture\\comic\\eva\\EVA_OneHalfLight.jpg",
-  window_background_image_hsb = {
-    brightness = 1.0,
-    hue = 1.0,
-    saturation = 1.0,
-  },
-
-  window_background_opacity = 1.0,
-  text_background_opacity = 0.90,
+  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+  window_padding = { left = 0, right = 0, top = 5, bottom = 0 },
+  enable_scroll_bar = false,
 
   exit_behavior = "Close",
   keys = mykeys,
+
+  front_end = "WebGpu",
 }
