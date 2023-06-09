2246a43f1ea23fef0273578e12bda0a145cef2f6
meijieru <meijieru@gmail.com>
2023-06-08 17:09:06 -0700

    fix(nvim): logging level

--- dot_config/lvim/lua/core/env_specific.lua
+++ dot_config/lvim/lua/core/env_specific.lua
@@ -2,14 +2,14 @@ local utils = require "core.utils"
 local Log = require "core.log"
 
 if utils.is_vscode() then
-  Log:info "In vscode frontend"
+  Log:debug "In vscode frontend"
   myvim.plugins.wakatime.active = false
   myvim.plugins.imtoggle.active = false
   myvim.plugins.cmp.active = false
 end
 
 if utils.is_neovide() then
-  Log:info "In neovide frontend"
+  Log:debug "In neovide frontend"
   vim.o.guifont = "JetBrainsMono Nerd Font Mono:h13"
   myvim.plugins.wakatime.active = false
 end
