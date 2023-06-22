b1d1c3be07cb1562d0d2aa236bcca56f590d7634
meijieru <meijieru@gmail.com>
2023-06-22 16:03:35 -0700

    fix(nvim): disable lsp for specific filetypes

--- dot_config/lvim/lua/core/defaults.lua
+++ dot_config/lvim/lua/core/defaults.lua
@@ -51,7 +51,7 @@ _G.myvim = {
     lvim = {
       skipped_servers = {},
       ensured_servers = { "ruff_lsp" },
-      ensured_filetypes = { "markdown", "rst", "plaintext", "toml", "proto" },
+      ensured_filetypes = { "toml" },
     },
   },
 
