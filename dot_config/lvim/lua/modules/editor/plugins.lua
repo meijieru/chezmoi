26dbed540a535d8dade5d847154e4a447a0c82fc
meijieru <meijieru@gmail.com>
2023-07-05 23:08:01 -0700

    feat(nvim): use flash

--- dot_config/lvim/lua/modules/editor/plugins.lua
+++ dot_config/lvim/lua/modules/editor/plugins.lua
@@ -50,6 +50,54 @@ M["kevinhwang91/nvim-ufo"] = {
   enabled = myvim.plugins.ufo.active,
 }
 
+M["folke/flash.nvim"] = {
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+    {
+      "R",
+      mode = { "o", "x" },
+      function()
+        require("flash").treesitter_search()
+      end,
+      desc = "Flash Treesitter Search",
+    },
+    {
+      "<c-s>",
+      mode = { "c" },
+      function()
+        require("flash").toggle()
+      end,
+      desc = "Toggle Flash Search",
+    },
+  },
+  enabled = myvim.plugins.flash.active,
+}
+
 M["ggandor/leap.nvim"] = {
   event = { "VeryLazy" },
   dependencies = {
