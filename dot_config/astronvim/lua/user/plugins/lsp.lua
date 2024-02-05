d280c9cddfe7bae12c1370ca7ee24e1b76316bc0
meijieru <meijieru@gmail.com>
2024-02-05 18:32:18 -0500

    chore(nvim): use ruff for range format

--- dot_config/astronvim/lua/user/plugins/lsp.lua
+++ dot_config/astronvim/lua/user/plugins/lsp.lua
@@ -8,17 +8,12 @@ return {
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    local yapf = null_ls.builtins.formatting.yapf
-    yapf.method = null_ls.methods.RANGE_FORMATTING
     opts.sources = {
       null_ls.builtins.formatting.stylua,
       null_ls.builtins.formatting.prettierd,
       null_ls.builtins.formatting.shfmt,
       null_ls.builtins.formatting.cmake_format,
       null_ls.builtins.code_actions.gitrebase,
-
-      -- TODO(meijieru): revisit when ruff works
-      yapf,
     }
     return opts
   end,
