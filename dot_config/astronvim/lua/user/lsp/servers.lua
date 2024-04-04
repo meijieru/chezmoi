60dcebbb67106de6fb038e97d4071f3971acc70e
meijieru <meijieru@gmail.com>
2024-04-03 21:06:55 -0700

    feat(nvim): use basedpyright

--- dot_config/astronvim/lua/user/lsp/servers.lua
+++ dot_config/astronvim/lua/user/lsp/servers.lua
@@ -2,7 +2,6 @@ if not myvim.plugins.is_development_machine then return {} end
 
 -- enable servers that you already have installed without mason
 return {
-  "pyright",
   "ruff_lsp",
   "clangd",
   "lua_ls",
