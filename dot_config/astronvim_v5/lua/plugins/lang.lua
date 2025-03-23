21d1abebf3d97eae4543df46aab01857491755d7
meijieru <meijieru@gmail.com>
2025-03-23 14:48:49 -0700

    fix(nvim): python dap

--- dot_config/astronvim_v5/lua/plugins/lang.lua
+++ dot_config/astronvim_v5/lua/plugins/lang.lua
@@ -1,31 +1,25 @@
-if true then
-  -- FIXME(meijieru): revisit after v5
-  return {}
-end
-
 return {
-
-  -- { import = "astrocommunity.pack.markdown" },
-
   {
     "jay-babu/mason-nvim-dap.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, "python")
-      if not opts.handlers then opts.handlers = {} end
-      opts.handlers.python = function() end -- make sure python doesn't get set up by mason-nvim-dap, it's being set up by nvim-dap-python
+      -- make sure python doesn't get set up by mason-nvim-dap, it's being set up by nvim-dap-python
+      opts.handlers = vim.tbl_deep_extend("force", opts.handlers or {}, {
+        python = function() end,
+      })
     end,
-    enabled = false,
   },
 
   {
     "mfussenegger/nvim-dap-python",
-    dependencies = "mfussenegger/nvim-dap",
-    ft = "python",
+    lazy = true,
     config = function(_, opts)
       local path = require("mason-registry").get_package("debugpy"):get_install_path() .. "/venv/bin/python"
       require("dap-python").setup(path, opts)
     end,
-    -- TODO(meijieru): slow down python file open
-    enabled = false,
+    specs = {
+      "mfussenegger/nvim-dap",
+      optional = true,
+      dependencies = "mfussenegger/nvim-dap-python",
+    },
   },
 }
