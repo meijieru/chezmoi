f4739823fb95cda1379aa041db23202eef2fcae4
meijieru <meijieru@gmail.com>
2025-06-12 12:31:55 -0700

    feat(nvim): conditionally import language packs for dev machines

--- dot_config/astronvim_v5/lua/community.lua
+++ dot_config/astronvim_v5/lua/community.lua
@@ -2,10 +2,17 @@
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- NOTE(meijieru): This guarantees that the specs are processed before any user plugins.
 
----@type LazySpec
-return {
+local spec = {
   "AstroNvim/astrocommunity",
 
   { import = "astrocommunity.editing-support.conform-nvim" },
   -- import/override with your plugins folder
 }
+
+if myvim.plugins.is_development_machine and not myvim.plugins.is_corporate_machine then
+  return vim.list_extend(spec, {
+    { import = "astrocommunity.pack.bash" },
+    { import = "astrocommunity.pack.json" },
+    { import = "astrocommunity.pack.markdown" },
+  })
+end
