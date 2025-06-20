8a4ec511ff3cdb663594ceab29efc2dbc3fd4e3d
meijieru <meijieru@gmail.com>
2025-06-20 11:16:02 -0700

    feat(nvim): add custom icons and highlights for CodeCompanion filetype in lang.lua

--- dot_config/astronvim_v5/lua/plugins/lang.lua
+++ dot_config/astronvim_v5/lua/plugins/lang.lua
@@ -41,6 +41,35 @@ return {
             },
           },
         },
+        filetype = {
+          -- https://github.com/olimorris/codecompanion.nvim/discussions/1638#discussioncomment-13484618
+          -- https://github.com/olimorris/codecompanion.nvim/discussions/1638#discussioncomment-13532012
+          codecompanion = {
+            heading = {
+              -- icons = { "󰪥 ", "  ", " ", " ", " ", "" },
+              custom = {
+                codecompanion_input = {
+                  pattern = "^## Me$",
+                  icon = " ",
+                },
+              },
+            },
+            html = {
+              tag = {
+                buf = { icon = " ", highlight = "CodeCompanionChatVariable" },
+                file = { icon = " ", highlight = "CodeCompanionChatVariable" },
+                help = { icon = "󰘥 ", highlight = "CodeCompanionChatVariable" },
+                image = { icon = " ", highlight = "CodeCompanionChatVariable" },
+                symbols = { icon = " ", highlight = "CodeCompanionChatVariable" },
+                url = { icon = "󰖟 ", highlight = "CodeCompanionChatVariable" },
+                var = { icon = " ", highlight = "CodeCompanionChatVariable" },
+                tool = { icon = " ", highlight = "CodeCompanionChatTool" },
+                user_prompt = { icon = " ", highlight = "CodeCompanionChatTool" },
+                group = { icon = " ", highlight = "CodeCompanionChatToolGroup" },
+              },
+            },
+          },
+        },
       },
     },
   },
